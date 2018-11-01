6bed21e2a161b5be16593a01f94d6b27b34af19f
BuildTools <unconfigured@null.spigotmc.org>
2018-11-01 07:48:59 -0400

    renaming package

--- Hocr/Properties/AssemblyInfo.cs
+++ Hocr/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SearchablePdf")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Net.FairfieldTek.Hocr")]
+[assembly: AssemblyDescription("Converts Scanned PDFS to searchable Pdfs")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SearchablePdf")]
+[assembly: AssemblyCompany("Fairfield Tek L.L.C.")]
+[assembly: AssemblyProduct("Hocr")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
