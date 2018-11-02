180adbb8befd8376110d2651690372c871ab47e1
Winterleaf <VGee@winterleafentertainment.com>
2018-11-02 11:01:56 -0400

    fixed multithreading issue with ghostscript

--- Hocr/ImageProcessors/GhostScript.cs
+++ Hocr/ImageProcessors/GhostScript.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+using System.Threading;
 using Ghostscript.NET;
 using Ghostscript.NET.Processor;
 using Net.FairfieldTek.Hocr.Enums;
@@ -11,56 +12,119 @@ namespace Net.FairfieldTek.Hocr.ImageProcessors
 {
     internal class GhostScript
     {
-        private readonly int _dpi;
-        
 
-        public GhostScript( int dpi)
+        internal class GhostScriptInstance : IDisposable
         {
-        
-            _dpi = dpi;
-        }
+            private int Innum {
+                get; set;
+            }
+            public GhostscriptProcessor GhostscriptProcessor {
+                get;
+            }
 
-        private static string DllFile {
-            get
+            public GhostScriptInstance()
             {
+                Innum = GhostScriptPool.Instance.GetInstance();
                 var assembly = Assembly.GetEntryAssembly();
-                if (assembly!=null && assembly.Location!=null)
+                string path=string.Empty;
+                if (assembly != null)
                     try
                     {
-                        return Path.Combine(Path.GetDirectoryName(assembly.Location), "dlls", BuildDetect.InternalCheckIsWow64() ? "gsdll32.dll" : "gsdll64.dll");
+                        path = Path.Combine(Path.GetDirectoryName(assembly.Location), "dlls", BuildDetect.InternalCheckIsWow64() ? $"gsdll32-{Innum}.dll" : $"gsdll64-{Innum}.dll");
                     }
                     catch (Exception e)
                     {
+                        path = Path.Combine(Environment.CurrentDirectory, "dlls", BuildDetect.InternalCheckIsWow64() ? $"gsdll32-{Innum}.dll" : $"gsdll64-{Innum}.dll");
                     }
-                    
-                return Path.Combine( Environment.CurrentDirectory,"dlls", BuildDetect.InternalCheckIsWow64() ? "gsdll32.dll" : "gsdll64.dll");
+
+                GhostscriptProcessor = new GhostscriptProcessor(new GhostscriptLibrary(new GhostscriptVersionInfo(path)));
             }
-    } 
+
+            public void Dispose()
+            {
+                GhostScriptPool.Instance.GiveInstance(Innum);
+                GhostscriptProcessor.Dispose();
+            }
+        }
+
+
+
+        internal class GhostScriptPool
+        {
+            private static readonly Lazy<GhostScriptPool> lazy = new Lazy<GhostScriptPool>(() => new GhostScriptPool(5));
+            public static GhostScriptPool Instance => lazy.Value;
+
+            private readonly Stack<int> _instances = new Stack<int>();
+            private GhostScriptPool(int maxInstance)
+            {
+                for (int i = 0; i <= maxInstance; i++)
+                    _instances.Push(i);
+            }
+
+            private readonly object _locker = new object();
+            public int GetInstance()
+            {
+                while (true)
+                {
+                    lock (_locker)
+                    {
+                        if (_instances.Count != 0)
+                            return (_instances.Pop());
+                        Thread.Sleep(10);
+                    }
+
+                }
+
+            }
+            public void GiveInstance(int i)
+            {
+                lock (_locker)
+                {
+                    _instances.Push(i);
+                }
+            }
+
+
+        }
+
+        private readonly int _dpi;
+
+
+        public GhostScript(int dpi)
+        {
+
+            _dpi = dpi;
+        }
+
+
 
         public string ConvertPdfToBitmap(string pdf, int startPageNum, int endPageNum, string sessionName)
         {
+
             string outPut = GetOutPutFileName(sessionName, ".bmp");
             pdf = "\"" + pdf + "\"";
 
-            GhostscriptProcessor proc = new GhostscriptProcessor(new GhostscriptLibrary(new GhostscriptVersionInfo(DllFile)));
+            //GhostscriptProcessor proc = new GhostscriptProcessor(new GhostscriptLibrary(new GhostscriptVersionInfo(DllFile)));
             List<string> switches = new List<string>
-            {
-                "-dNOPAUSE",
-                "-q",
-                $"-r{_dpi}",
-                "-sDEVICE=bmp16m",
-                "-dBATCH",
-                "-dGraphicsAlphaBits=4",
-                "-dTextAlphaBits=4",
-                $"-dFirstPage={startPageNum}",
-                $"-dLastPage={endPageNum}",
-                $"-sOutputFile={outPut}",
-                pdf,
-                "-c",
-                "quit"
-            };
-            proc.Process(switches.ToArray());
+                {
+                    "-dNOPAUSE",
+                    "-q",
+                    $"-r{_dpi}",
+                    "-sDEVICE=bmp16m",
+                    "-dBATCH",
+                    "-dGraphicsAlphaBits=4",
+                    "-dTextAlphaBits=4",
+                    $"-dFirstPage={startPageNum}",
+                    $"-dLastPage={endPageNum}",
+                    $"-sOutputFile={outPut}",
+                    pdf,
+                    "-c",
+                    "quit"
+                };
+            using (var gs = new GhostScriptInstance())
+                gs.GhostscriptProcessor.Process(switches.ToArray());
             return new FileInfo(outPut.Replace('"', ' ').Trim()).FullName;
+
         }
 
         private static string GetOutPutFileName(string sessionName, string extWithDot)
@@ -73,6 +137,7 @@ namespace Net.FairfieldTek.Hocr.ImageProcessors
             dPdfSettings dPdfSettings = dPdfSettings.screen,
             string options = "")
         {
+
             string clevel;
             switch (level)
             {
@@ -98,24 +163,26 @@ namespace Net.FairfieldTek.Hocr.ImageProcessors
             string outPutFileName = TempData.Instance.CreateTempFile(sessionName, ".pdf");
 
 
-            GhostscriptProcessor proc = new GhostscriptProcessor(new GhostscriptLibrary(new GhostscriptVersionInfo(DllFile)));
+            //GhostscriptProcessor proc = new GhostscriptProcessor(new GhostscriptLibrary(new GhostscriptVersionInfo(DllFile)));
             List<string> switches = new List<string>
-            {
-                "-q",
-                "-dNOPAUSE",
-                "-dBATCH",
-                "-dSAFER",
-                "-sDEVICE=pdfwrite",
-                $"-dCompatibilityLevel={clevel}",
-                $"-dPDFSETTINGS=/{dPdfSettings}",
-                options,
-                $"-sOutputFile={'"'}{outPutFileName}{'"'}",
-                $"{'"'}{inputPdf}{'"'}",
-                "-c",
-                "quit"
-            };
-            proc.Process(switches.ToArray());
+                {
+                    "-q",
+                    "-dNOPAUSE",
+                    "-dBATCH",
+                    "-dSAFER",
+                    "-sDEVICE=pdfwrite",
+                    $"-dCompatibilityLevel={clevel}",
+                    $"-dPDFSETTINGS=/{dPdfSettings}",
+                    options,
+                    $"-sOutputFile={'"'}{outPutFileName}{'"'}",
+                    $"{'"'}{inputPdf}{'"'}",
+                    "-c",
+                    "quit"
+                };
+            using (var gs = new GhostScriptInstance())
+                gs.GhostscriptProcessor.Process(switches.ToArray());
             return outPutFileName;
+
         }
     }
 }
